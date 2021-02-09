733fc2811c685d5e5f2f1194c868d0916a5dc646
Kyle Weekley <krw1619@g.rit.edu>
2021-02-09 07:43:14 -0500

    Added Rudy's CameraShake script and a cube for testing
    Use the TestShake bool on the MainCamera to try camera shakes with different durations and magnitudes.

---
+++ Assets/Scripts/CameraShake.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShake : MonoBehaviour
+{
+    private bool isShaking = false;
+    public bool testShake = false;
+    public float testDuration;
+    public float testMagnitude;
+
+    //Test shaking behavior using values in editor
+    private void Update()
+    {
+        if (testShake)
+        {
+            ShakeCamera(testDuration, testMagnitude);
+            testShake = false;
+        }
+    }
+
+    public void ShakeCamera(float duration, float magnitude)
+    {
+        if (!isShaking)
+            StartCoroutine(Shake(duration, magnitude));
+    }
+
+    private IEnumerator Shake(float duration, float magnitude)
+    {
+        isShaking = true;
+
+        Vector3 originalPos = transform.localPosition;
+
+        float elapsed = 0.0f;
+
+        while (elapsed < duration)
+        {
+            float x = Random.Range(-1f, 1f) * magnitude;
+            float y = Random.Range(-1f, 1f) * magnitude;
+
+            transform.localPosition = new Vector3(x, y, originalPos.z);
+
+            elapsed += Time.deltaTime;
+
+            yield return null;
+        }
+
+        transform.localPosition = originalPos;
+
+        isShaking = false;
+    }
+}
