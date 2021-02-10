30e0a434a1c6ce5867743a633d92c38c9d2d45fe
Nuha Syed <nqs3694@rit.edu>
2021-02-10 12:53:40 -0500

    Press space to make arrow shoot

---
+++ Assets/Projectile.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float speed = 20f;
+    public Rigidbody2D projectileBody;
+    public int dmg = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //rigidbody move right according to speed
+        projectileBody.velocity = transform.right * speed;
+        Destroy(gameObject, 2f);
+    }
+
+    //when bullet hits enemy
+    void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        //gets info of object we hit
+        Debug.Log(hitInfo.name);
+        //once we have an enemy time this will call it's take damage function
+        /*Enemy enemy = hitInfo.GetComponent<Enemy>();
+        if (enemy != null)
+        {
+            enemy.TakeDamage(dmg);
+        }*/
+
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Shoot.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform arrowPoint;
+    public GameObject arrowPrefab;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ShootArrow();
+        }
+    }
+    void ShootArrow ()
+    {
+        //shooting logic
+        Instantiate(arrowPrefab, arrowPoint.position, arrowPoint.rotation);
+    }
+}
