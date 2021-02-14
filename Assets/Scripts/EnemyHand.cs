1584efbf6f4df9bd163b8a4210457f967daf2f89
Kyle Weekley <krw1619@g.rit.edu>
2021-02-14 15:47:33 -0500

    Added enemy behavior
    Kyle Test Scene contains two hand enemies.
    They have two attacks:
    1. horizontal swipe
    2. vertical slam
    I haven't made any changes to player behavior, so the "fight" isn't balanced and there are wacky physics interactions with the player's rigidbody

---
+++ Assets/Scripts/EnemyHand.cs
@@ -0,0 +1,236 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Kyle Weekley
+// Governs movement and attack behavior for hand enemies
+
+public class EnemyHand : MonoBehaviour
+{
+    public enum HandState
+    {
+        Move,
+        HorizontalSwipe,
+        VerticalSlam
+    }
+
+    public HandState currentState;
+
+    public GameObject player;
+    public GameObject movementZone;
+    public GameObject ground;
+    private BoxCollider2D movementZoneBox;
+    private BoxCollider2D groundBox;
+
+    private BoxCollider2D handBox;
+
+    public float movementSpeedHorizontal;
+    public float movementSpeedVertical;
+    [SerializeField] private bool movingRight;
+    [SerializeField] private bool movingUp;
+    private bool atHorizontalEdge;
+
+    private float verticalSlamRange = 0.1f;
+    private float verticalSlamSpeed = 8f;
+
+    private float horizontalSwipeYSpeed = 4f;
+    private float horizontalSwipeXSpeed = 8f;
+
+    [SerializeField] private float actionTimer = 0f;
+    private float minimumMoveDuration = 1f;
+    private float verticalSlamDuration = 2f;
+
+    System.Random rnd;
+    private float horizontalSwipeChance = 50f;
+    private float verticalSlamChance = 75f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        movementZoneBox = movementZone.GetComponent<BoxCollider2D>();
+        groundBox = ground.GetComponent<BoxCollider2D>();
+        handBox = GetComponent<BoxCollider2D>();
+        rnd = new System.Random();
+        atHorizontalEdge = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Generate a number from 0-99 for use in attack chance rolls
+        int randomAction = rnd.Next(0, 100);
+
+        //When the hand is moving, check for valid attack conditions
+        if (currentState == HandState.Move)
+        {
+            //Only perform an attack if enough time has passed since the last attack attempt
+            if (actionTimer >= minimumMoveDuration) 
+            {       
+                if (atHorizontalEdge)
+                {
+                    actionTimer = 0f;
+
+                    //Execute the attack a specified percentage of the time
+                    if (randomAction < horizontalSwipeChance)
+                        HorizontalSwipe();
+                }
+                else if (DetectPlayerBelow())
+                {
+                    actionTimer = 0f;
+
+                    //Execute the attack a specified percentage of the time
+                    if (randomAction < verticalSlamChance)
+                    VerticalSlam();
+                }
+                else
+                {
+                    Move();
+                }
+            }
+            else
+            {
+                Move();
+            }
+        }
+        //If currently in an attacking state, continue that attack
+        else if (currentState == HandState.VerticalSlam)
+        {
+            VerticalSlam();
+        }
+        else if (currentState == HandState.HorizontalSwipe)
+        {
+            HorizontalSwipe();
+        }
+        
+    }
+
+    //Move back and forth in a zigzag pattern
+    public void Move()
+    {
+        atHorizontalEdge = false;
+        actionTimer += Time.deltaTime;
+        
+        //Horizontal movement section
+        if (movingRight)
+        {
+            transform.position += new Vector3(movementSpeedHorizontal * Time.deltaTime, 0, 0);
+
+            if (handBox.bounds.max.x > movementZoneBox.bounds.max.x)
+            {
+                movingRight = false;
+                atHorizontalEdge = true;
+            }
+        }
+        else
+        {
+            transform.position -= new Vector3(movementSpeedHorizontal * Time.deltaTime, 0, 0);
+
+            if (handBox.bounds.min.x < movementZoneBox.bounds.min.x)
+            {
+                movingRight = true;
+                atHorizontalEdge = true;
+            }
+        }
+
+        //Vertical movement section
+        if (movingUp)
+        {
+            transform.position += new Vector3(0, movementSpeedVertical * Time.deltaTime, 0);
+
+            if (handBox.bounds.max.y > movementZoneBox.bounds.max.y)
+            {
+                movingUp = false;
+            }
+        }
+        else
+        {
+            transform.position -= new Vector3(0, movementSpeedVertical * Time.deltaTime, 0);
+
+            if (handBox.bounds.min.y < movementZoneBox.bounds.min.y)
+            {
+                movingUp = true;
+            }
+        }
+    }
+
+    //Return true if the player is located below this hand
+    public bool DetectPlayerBelow()
+    {
+        if (handBox.bounds.min.y > player.transform.position.y && Mathf.Abs(transform.position.x - player.transform.position.x) <= verticalSlamRange)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    //Execute horizontal swipe attack
+    public void HorizontalSwipe()
+    {
+        currentState = HandState.HorizontalSwipe;
+
+        //Step 1: The hand lowers itself to just above the ground
+        if (handBox.bounds.min.y > groundBox.bounds.max.y)
+        {
+            transform.position -= new Vector3 (0, horizontalSwipeYSpeed * Time.deltaTime, 0);
+        }
+
+        //Step 2: The hand moves quickly along the floor until it reaches the other side of the arena
+        else if (movingRight)
+        {
+            transform.position += new Vector3 (horizontalSwipeXSpeed * Time.deltaTime, 0, 0);
+
+            //Once the hand reaches the other side of the arena, return to moving state
+            if (handBox.bounds.max.x >= movementZoneBox.bounds.max.x)
+            {
+                currentState = HandState.Move;
+                actionTimer = 0f;
+            }
+        }
+        else if (!movingRight)
+        {
+            transform.position -= new Vector3 (horizontalSwipeXSpeed * Time.deltaTime, 0, 0);
+
+            //Once the hand reaches the other side of the arena, return to moving state
+            if (handBox.bounds.min.x <= movementZoneBox.bounds.min.x)
+            {
+                currentState = HandState.Move;
+                actionTimer = 0f;
+            }
+        }
+    }
+
+    //Execute vertical slam attack
+    public void VerticalSlam()
+    {
+        currentState = HandState.VerticalSlam;
+
+        //Step 3: The hand rests on the ground for a moment, allowing the player to counterattack
+        actionTimer += Time.deltaTime;
+        if (handBox.bounds.min.y <= groundBox.bounds.max.y)
+        {
+            //Animate, probably
+        }
+
+        //Step 1: The hand rears up, rising a little
+        else if (actionTimer <= verticalSlamDuration * 0.25f)
+        {
+            transform.position += new Vector3(0, verticalSlamSpeed / 4 * Time.deltaTime, 0);
+        }
+
+        //Step 2: The hand quickly slams down until it strikes the ground
+        else
+        {
+            transform.position -= new Vector3(0, verticalSlamSpeed * Time.deltaTime, 0);
+        }
+
+        //Once the attck's duration has been reached, return to moving state
+        if (actionTimer >= verticalSlamDuration)
+        {
+            currentState = HandState.Move;
+            actionTimer = 0f;
+        }
+    }
+}
