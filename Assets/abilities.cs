3ce4fda4b5c1f0ddaba3cb05478f65696b6af625
Nuha Syed <nqs3694@rit.edu>
2021-02-15 17:33:35 -0500

    UI Time go brr when press left shift

---
+++ Assets/abilities.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class abilities : MonoBehaviour
+{
+
+    public Image abilityImage1;
+    public float cooldown = 20;
+    bool isCooldown = false;
+    public KeyCode ability1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        abilityImage1.fillAmount = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ability1();
+    }
+
+    void Ability1()
+    {
+        if(Input.GetKey(ability1) && isCooldown == false)
+        {
+            isCooldown = true;
+            abilityImage1.fillAmount = 1;
+        }
+
+        if (isCooldown)
+        {
+            abilityImage1.fillAmount -= 1 / cooldown * Time.deltaTime;
+
+            if (abilityImage1.fillAmount <= 0)
+            {
+                abilityImage1.fillAmount = 0;
+                isCooldown = false;
+            }
+        }
+    }
+}
